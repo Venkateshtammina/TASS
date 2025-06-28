57da7daae2b204dc41257126f16ed8cb1e9cc681
Venkatesh T <venkatesht1243@gmail.com>
2025-06-28 14:22:12 +0530

    make it deployment ready

--- backend/main.py
+++ backend/main.py
@@ -176,4 +176,6 @@ def get_traffic_news():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
